37a911100eba9c4b4c00ec3cbc7e444959bfea4d
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2025-11-09 22:10:13 +0600

    Little Modified

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,11 @@ const express_1 = __importDefault(require("express"));
 const book_controller_1 = require("./app/controllers/book.controller");
 const borrow_controller_1 = require("./app/controllers/borrow.controller");
 const errorHandler_1 = require("./app/middlewares/errorHandler");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    origin: ['http://localhost:5173']
+}));
 app.use(express_1.default.json());
 app.get('/', (req, res) => {
     res.send('Welcome to Library Management System API');
--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,19 @@ import express, { Application, Request, Response  } from "express";
 import { bookRoutes } from "./app/controllers/book.controller";
 import { borrowRoutes } from "./app/controllers/borrow.controller";
 import { errorHandler } from "./app/middlewares/errorHandler";
+import cors from 'cors';
 
 
 
 
 const app: Application = express();
+
+app.use(
+  cors({
+
+    origin: ['http://localhost:5173']
+   })
+);
 
 app.use(express.json());
 
